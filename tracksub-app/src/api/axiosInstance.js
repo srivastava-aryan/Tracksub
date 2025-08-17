8ea8c696c827e375547bcf203d0c92dfc914865c
srivastava-aryan <aru93295@gmail.com>
2025-08-17 22:14:01 +0530

    inita

--- tracksub-app/src/api/axiosInstance.js
+++ tracksub-app/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // backend API base
+  // baseURL: "http://localhost:5000/api", 
+  baseURL:"https://tracksub-2u8x.onrender.com/api"
 });
 
 export default instance;
