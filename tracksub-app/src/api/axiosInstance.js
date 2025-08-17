2ba6c3f57eab5a946f259ca3bccf814377a4afde
srivastava-aryan <aru93295@gmail.com>
2025-08-17 22:19:38 +0530

    added env

--- tracksub-app/src/api/axiosInstance.js
+++ tracksub-app/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:5000/api", 
-  baseURL:"https://tracksub-2u8x.onrender.com/api"
+   baseURL: "http://localhost:5000/api", 
+  // baseURL:"https://tracksub-2u8x.onrender.com/api"
 });
 
 export default instance;
--- tracksub-app/src/firebase/config.js
+++ tracksub-app/src/firebase/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ4z7ciGcINlc2SzThwKvJ9BtYQAbmrCg",
-  authDomain: "tracksub-app.firebaseapp.com",
-  projectId: "tracksub-app",
-  storageBucket: "tracksub-app.firebasestorage.app",
-  messagingSenderId: "35598700280",
-  appId: "1:35598700280:web:4b0a597cb87853d3ebb50d"
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID
 };
 
 // Initialize Firebase
