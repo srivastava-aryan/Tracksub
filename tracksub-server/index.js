8267344d74213b8dfb83a2a725e3b284f7f01500
srivastava-aryan <aru93295@gmail.com>
2025-08-19 22:14:50 +0530

    Fix : instance err

--- tracksub-server/index.js
+++ tracksub-server/index.js
@@ -6,20 +6,14 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://tracksub-app.onrender.com"],
-    credentials: true,
-  })
-);
+app.use(cors(({ origin: ["http://localhost:5173", "https://tracksub-app.onrender.com"], credentials: true })));
 app.use(express.json());
 
 const subscriptionRoutes = require("./routes/subscriptionRoutes");
 app.use("/api/subscriptions", subscriptionRoutes);
 
 // MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
