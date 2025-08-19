39fde199f1e00ac229c67fd975388901e634839e
srivastava-aryan <aru93295@gmail.com>
2025-08-19 16:05:31 +0530

    added color bg

--- tracksub-app/src/pages/Home.jsx
+++ tracksub-app/src/pages/Home.jsx
@@ -1,4 +1,8 @@
 const Home = () => {
-  return <div className="p-4 text-xl font-semibold">🏠 Welcome to SubTrack</div>;
+  return (
+    <div className="min-h-screen p-4 text-xl font-semibold bg-gray-100 dark:bg-gray-900 transition-colors">
+      🏠 Welcome to SubTrack
+    </div>
+  );
 };
 export default Home;
